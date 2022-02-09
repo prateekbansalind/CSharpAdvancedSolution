01ed039b497aa4953a52736184d082651409d2d6
Prateek Bansal <prateekbansalind@gmail.com>
2022-02-10 05:34:44 +1300

    first iteration add LambdaExpression.cs

---
+++ LambdaExpressions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LambdaExpressions
+{
+    public class Program
+    {
+        // what is a Lambda Expression
+
+        // An anonymous method
+        // No access modifier
+        // No name
+        // No return statement
+
+        // why do we use them - for convience
+        static void Main(string[] args)
+        {
+            // args => expression 
+            // number => number * number;
+            // () => ...
+            // x => ...
+            // (x, y, z) => ...
+            Func<int, int> square = number => number * number;
+
+            //Console.WriteLine(Square(5));
+            Console.WriteLine(square(5));
+
+            const int factor = 7;
+            Func<int, int> multiplier = n => n * factor;
+            var result = multiplier(16);
+            Console.WriteLine(result);
+        }
+
+        //static int Square(int number)
+        //{
+        //    return number * number; 
+        //}
+    }
+}
